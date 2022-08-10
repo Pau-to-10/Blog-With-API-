d98e0f08d70b0573ae97dd5b4c8db2b0efe0c212
Pau Tomas <tomas10pau@gmail.com>
2022-08-10 18:05:07 +0200

    avances hasta 10/08/2022

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const API_URL = "http://localhost:3000";
-
+ 
 const xhr = new XMLHttpRequest();
 
 function onRequestHandler() {
     if(this.readyState ===4 && this.status === 200){
-       console.log(this.response);
+       const data = JSON.parse(this.response);
+       const HTMLResponse = document.querySelector('#container');
+        
+       const tpl = data.map((post) => `<a class="title-post" data-toggle="modal" data-target="#exampleModalCenter""><li>${post.title}</li></a>`);
+       HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
     }
 }
 
